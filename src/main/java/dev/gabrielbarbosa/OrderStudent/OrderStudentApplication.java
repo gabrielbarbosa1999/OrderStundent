4772f9b79db0741c495bf90b82b0156389c16d74
Gabriel Barbosa <gabriel.barbosa@myorgtech.com>
2023-07-26 22:09:49 -0300

    Criado entrada um

--- src/main/java/dev/gabrielbarbosa/OrderStudent/OrderStudentApplication.java
+++ src/main/java/dev/gabrielbarbosa/OrderStudent/OrderStudentApplication.java
@@ -1,5 +1,8 @@
 package dev.gabrielbarbosa.OrderStudent;
 
+import dev.gabrielbarbosa.OrderStudent.entities.Order;
+import dev.gabrielbarbosa.OrderStudent.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +10,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrderStudentApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrderStudentApplication.class, args);
-    }
+	@Autowired
+	private OrderService orderService;
 
-    @Override
-    public void run(String... args) throws Exception {
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStudentApplication.class, args);
+	}
 
-    }
+	@Override
+	public void run(String... args) throws Exception {
+
+		//Entrada 1
+		Order orderOne = new Order(1034, 150.00, 20.0);
+		System.out.println("Pedido código " + orderOne.getCode());
+		System.out.println("Valor total: R$" + orderService.total(orderOne));
+
+	}
 
 }
